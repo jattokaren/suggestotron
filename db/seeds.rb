a0a048f27d239d039fe54681a440ec0819b4fe39
Joe Attok <jattokaren@gmail.com>
2018-02-09 20:11:50 -0800

    Added .SCSS and tried a .csv seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'example.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Topics.new
+  t.id = row['id']
+  t.title = row['title']
+  t.description = row['description']
+  t.created_at = row['created_at']
+  t.updated_at = row['updated_at']
+  t.save
+  puts "#{t.title}, #{t.description} saved"
+end
+
+puts "There are now #{Topics.count} rows in the topics table"
